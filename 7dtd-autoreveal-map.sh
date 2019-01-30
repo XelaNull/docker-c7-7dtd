437705d72fbf6b32f31e75f1089f68a73da4c904
XelaNull <xelanull@orbitalgrooves.com>
2019-01-30 16:09:45 -0500

    changed filename

--- 7dtd-autoreveal-map.sh
+++ 7dtd-autoreveal-map.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/expect
+# Syntax: ./7dtd-rendermap.sh <PlayerName> <StartingCoordinate> <EndingCoordinate>
 set timeout 5; set first_height 200; set second_height 0; set viewed_block_size 140; set sleep 2; set count 0; 
 spawn telnet localhost 8081; expect "Please enter password:"; send "sanity\r";
 for {set y $2} {$y < $3} {incr y $viewed_block_size} {
