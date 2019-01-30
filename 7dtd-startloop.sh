49dd9ad0dc55229cc980c17e6bcf3d57bb1c29af
XelaNull <xelanull@orbitalgrooves.com>
2019-01-30 15:21:39 -0500

    bug fixes

--- 7dtd-startloop.sh
+++ 7dtd-startloop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ONLY RUN THIS SCRIPT IF WE HAVENT RUN IT BEFORE
-[[ ! -z /startloop.touch ]] && exit
+[[ -f /startloop.touch ]] && exit
 
 # DELAY START TO GIVE 7DTD SERVER A CHANCE TO START
 sleep 30;
@@ -11,17 +11,18 @@ sleep 30;
 [[ -z $INSTALL_DIR ]] && export INSTALL_DIR=/data/7DTD
 export RADIATION_BORDER_WIDTH=350
 
-# ENABLE MAP RENDERING OF VISITED LOCATIONS
-/7dtd-sendcmd.sh "rendermap\renablerendering\r"
-
 # LOOP UNTIL FIRST PLAYER JOINS SERVER
-while true:
+while true
+do
   # Player connected, entityid=171, name=PlayerName, steamid=1111111111, steamOwner=1111111111, ip=xxxxxxx
-  PLAYERNAME=`grep 'Player connected' $INSTALL_DIR/7dtd.log | head -1 | awk '{print $4}' | cut -d= -f2`
+  PLAYERNAME=`grep 'Player connected' $INSTALL_DIR/7dtd.log | head -1 | awk '{print $7}' | cut -d, -f1 | cut -d= -f2`
   [[ ! -z $PLAYERNAME ]] && break;
   # Sleep for 2 seconds, then repeat
   sleep 2
 done
+
+# ENABLE MAP RENDERING OF VISITED LOCATIONS
+/7dtd-sendcmd.sh "rendermap\renablerendering\r"
 
 # MAKE FIRST PLAYER AN ADMIN
 /7dtd-sendcmd.sh "admin add $PLAYERNAME 0\r"
