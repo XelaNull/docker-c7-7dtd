5f5f1f004d5312b909451dc9848000dd5733059f
XelaNull <xelanull@orbitalgrooves.com>
2019-02-02 09:44:40 -0500

    fixed typo

--- install_7dtd.sh
+++ install_7dtd.sh
@@ -8,6 +8,9 @@ if [[ -z $STEAMCMD_LOGIN ]] || [[ -z $STEAMCMD_PASSWORD ]] || [[ -z $STEAMCMD_AP
 fi
 set -e
 
+# Erase Existing Application directory
+rm -rf $INSTALL_DIR
+
 # Set up the installation directory
 [[ ! -d $INSTALL_DIR ]] && mkdir -p $INSTALL_DIR/html; cp /index.php $INSTALL_DIR/html/
 chown steam:steam $INSTALL_DIR /home/steam -R
@@ -30,15 +33,16 @@ unzip -o CSMM_Patrons.zip
 rm -rf 7DTD-ScriptingMod && git clone https://github.com/djkrose/7DTD-ScriptingMod
 cp /COMPOPACK_35.zip $INSTALL_DIR && cd $INSTALL_DIR && unzip COMPOPACK_35.zip && \
 cp COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Prefabs/* Data/Prefabs/ && \
-cp rm -rf Data/Config/rwgmixer.xml && COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Config/rwgmixer.xml Data/Config/
+rm -rf Data/Config/rwgmixer.xml && cp COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Config/rwgmixer.xml Data/Config/
 # CSMM Map Addon
 wget -O map.js "https://confluence.catalysm.net/download/attachments/1114446/map.js?version=1&modificationDate=1548000113141&api=v2&download=true" && \
 mv map.js $INSTALL_DIR/Mods/Allocs_WebAndMapRendering/webserver/js
 #rm -rf 7dtd-ServerTools-12.7.zip; wget https://github.com/dmustanger/7dtd-ServerTools/releases/download/12.7/7dtd-ServerTools-12.7.zip
 
-
-echo "Copying in cloned copy of auto-reveal map"
-cp -rp /7dtd-auto-reveal-map $INSTALL_DIR
+if [ -d /7dtd-auto-reveal-map ]; then
+  echo "Copying in cloned copy of auto-reveal map"
+  cp -rp /7dtd-auto-reveal-map $INSTALL_DIR
+fi
 
 echo "Applying CUSTOM CONFIGS against application default files" && /7dtd-APPLY-CONFIG.sh
 chown steam:steam $INSTALL_DIR /home/steam -R
