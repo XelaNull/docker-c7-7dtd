6e85fd9d6c4a568429b534fc030bf65175e9fa53
XelaNull <xelanull@orbitalgrooves.com>
2019-02-02 09:37:30 -0500

    installed all except ServerTools

--- install_7dtd.sh
+++ install_7dtd.sh
@@ -24,17 +24,18 @@ sudo -u steam /home/steam/steamcmd.sh +login $STEAMCMD_LOGIN $STEAMCMD_PASSWORD 
 
 # Install MODS
 echo "Installing 7DTD Mods"
-#cd $INSTALL_DIR; rm -rf Botman_Mods_A17.zip; wget http://botman.nz/Botman_Mods_A17.zip && unzip -o Botman_Mods_A17.zip
-#cd $INSTALL_DIR/Mods; rm -rf CSMM_Patrons.zip; wget -O CSMM_Patrons.zip https://confluence.catalysm.net/download/attachments/1114182/CSMM_Patrons_9.0.zip?api=v2
-#unzip -o CSMM_Patrons.zip
-#rm -rf 7DTD-ScriptingMod && git clone https://github.com/djkrose/7DTD-ScriptingMod
-rm -rf 7dtd-ServerTools-12.7.zip; wget https://github.com/dmustanger/7dtd-ServerTools/releases/download/12.7/7dtd-ServerTools-12.7.zip
-#cp /COMPOPACK_35.zip $INSTALL_DIR && cd $INSTALL_DIR && unzip COMPOPACK_35.zip && \
-#cp COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Prefabs/* Data/Prefabs/ && \
-#cp rm -rf Data/Config/rwgmixer.xml && COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Config/rwgmixer.xml Data/Config/
+cd $INSTALL_DIR; rm -rf Botman_Mods_A17.zip; wget http://botman.nz/Botman_Mods_A17.zip && unzip -o Botman_Mods_A17.zip
+cd $INSTALL_DIR/Mods; rm -rf CSMM_Patrons.zip; wget -O CSMM_Patrons.zip https://confluence.catalysm.net/download/attachments/1114182/CSMM_Patrons_9.0.zip?api=v2
+unzip -o CSMM_Patrons.zip
+rm -rf 7DTD-ScriptingMod && git clone https://github.com/djkrose/7DTD-ScriptingMod
+cp /COMPOPACK_35.zip $INSTALL_DIR && cd $INSTALL_DIR && unzip COMPOPACK_35.zip && \
+cp COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Prefabs/* Data/Prefabs/ && \
+cp rm -rf Data/Config/rwgmixer.xml && COMPOPACK_35\(for\ Alpha17exp_b233\)/data/Config/rwgmixer.xml Data/Config/
+# CSMM Map Addon
+wget -O map.js "https://confluence.catalysm.net/download/attachments/1114446/map.js?version=1&modificationDate=1548000113141&api=v2&download=true" && \
+mv map.js $INSTALL_DIR/Mods/Allocs_WebAndMapRendering/webserver/js
+#rm -rf 7dtd-ServerTools-12.7.zip; wget https://github.com/dmustanger/7dtd-ServerTools/releases/download/12.7/7dtd-ServerTools-12.7.zip
 
-# https://confluence.catalysm.net/download/attachments/1114446/map.js?version=1&modificationDate=1548000113141&api=v2&download=true
-# install to:  \Mods\Allocs_WebAndMapRendering\webserver\js
 
 echo "Copying in cloned copy of auto-reveal map"
 cp -rp /7dtd-auto-reveal-map $INSTALL_DIR
