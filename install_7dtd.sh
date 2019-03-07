30f06b5c2407a3e14e4753a7348abc382aa335bf
XelaNull <xelanull@orbitalgrooves.com>
2019-03-07 12:11:10 -0500

    don't delete the installation directory!

--- install_7dtd.sh
+++ install_7dtd.sh
@@ -9,7 +9,7 @@ fi
 set -e
 
 # Erase Existing Application directory
-rm -rf $INSTALL_DIR
+# rm -rf $INSTALL_DIR
 
 # Set up the installation directory
 [[ ! -d $INSTALL_DIR/.local ]] && mkdir -p $INSTALL_DIR/.local; 
@@ -26,11 +26,14 @@ echo "Starting Steam to perform application install"
 su steam -c "/home/steam/steamcmd.sh +login $STEAMCMD_LOGIN $STEAMCMD_PASSWORD \
   +force_install_dir $INSTALL_DIR +app_update $STEAMCMD_APP_ID \
   $beta $betapassword $validate +quit"
-
+  
+# Install 7DTD ServerMod Manager & Download Mods
 cd $INSTALL_DIR; [[ -d $INSTALL_DIR/7dtd-servermod ]] && rm -rf 7dtd-servermod
 git clone https://github.com/XelaNull/7dtd-servermod.git && \
 cd 7dtd-servermod && chmod a+x install_mods.sh && ./install_mods.sh $INSTALL_DIR
 
 chown steam:steam $INSTALL_DIR /home/steam -R
 #echo "Stopping 7DTD to kick off new world generation (if name changes)" && /stop_7dtd.sh
-echo "Completed Installation."; touch /7dtd.initialized; exec "$@"
+echo "Completed Installation."; touch /7dtd.initialized; 
+$IPADDRESS=exec("/sbin/ifconfig | grep broad | awk '{print \$2}'");
+echo "Your server should now be accessible at: http://$IPADDRESS/7dtd"; exec "$@"
