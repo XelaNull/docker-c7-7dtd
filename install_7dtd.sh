d4b330dc2b2f47670909ec7e3bac0b45bf88923b
XelaNull <xelanull@orbitalgrooves.com>
2020-01-12 22:15:09 -0500

    enable servermod manager

--- install_7dtd.sh
+++ install_7dtd.sh
@@ -31,7 +31,8 @@ su steam -c "/home/steam/steamcmd.sh +login $STEAMCMD_LOGIN $STEAMCMD_PASSWORD \
 echo 'export INSTALL_DIR=/data/7DTD; cd $INSTALL_DIR; [[ -d $INSTALL_DIR/7dtd-servermod ]] && rm -rf 7dtd-servermod' > /install_servermodmgr.sh
 echo 'git clone https://github.com/XelaNull/7dtd-servermod.git && \' >> /install_servermodmgr.sh
 echo 'cd 7dtd-servermod && chmod a+x install_mods.sh && ./install_mods.sh $INSTALL_DIR' >> /install_servermodmgr.sh
-chmod a+x /install_servermodmgr.sh
+chmod a+x /install_servermodmgr.sh 
+/install_servermodmgr.sh 
 
 chown steam:steam $INSTALL_DIR /home/steam -R
 #echo "Stopping 7DTD to kick off new world generation (if name changes)" && /stop_7dtd.sh
